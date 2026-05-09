663ff0da3b8ef8d15f1dd86794d69f501d7aab5d
Eimantas <eimantuske59@gmail.com>
2026-05-09 18:10:06 +0300

    Add xorSifravimas function and header file for encryption

---
+++ include/shifravimas.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+std::string xorSifravimas(const std::string& duomenys, const std::string& raktas);
+
+bool apdorotiFaila(const std::string& failoKelias, const std::string& raktas);
---
+++ src/shifravimas.cpp
@@ -0,0 +1,12 @@
+#include "shifravimas.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+string xorSifravimas(const string& duomenys, const string& raktas) {
+    if(raktas.empty()) 
+        return duomenys;
+}
+
+
